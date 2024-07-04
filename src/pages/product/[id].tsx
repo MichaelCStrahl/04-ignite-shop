2a01405959a362047649b37d4b16b1c1e42ce903
Michael Strahl <michaelstrahl.sm@gmail.com>
2024-07-04 15:39:01 -0300

    create product page style

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const { query } = useRouter()
-  return <h1>Product: {JSON.stringify(query)}</h1>
+
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odit,
+          exercitationem molestiae voluptatem, dignissimos laboriosam alias
+          fugit, dolor molestias praesentium fuga? Nesciunt, alias suscipit
+          quaerat excepturi aliquid molestias nostrum autem!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
